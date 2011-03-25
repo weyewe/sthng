bf3f4dcc19aa5e655613d30142830f5fde590259
weyewe <weyewe@Willy-Yunnals-MacBook.local>
2011-03-25 21:10:47 +0700

    basic

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilihfoto::Application.config.secret_token = 'f3b4eb06043942da871c0e640ea4b249e1c14aa8a64ce7e9874cb2361fe1091e3b8aa504f1502fbac55c2fe03866e46eb476fc38e1d109cd3ecef4e4539ff267'
